cfc9f044630d1d97faa91047e004d63ab509413a
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-03-10 01:16:33 +0530

    [Feature 2] Implement 'cat-file -p' command to view contents of blobs in Git

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib");
 
 const command = process.argv[2];
 
 switch (command) {
   case "init":
     createGitDir();
+    break;
+  case "cat-file":
+    catFile();
     break;
   default:
     throw new Error(`Unknown command: ${command}`);
@@ -21,4 +25,20 @@ function createGitDir() {
     "ref: refs/heads/master\n"
   );
   console.log("Initialized an empty directory");
+}
+
+async function catFile() {
+  if (process.argv[3] !== "-p") {
+    throw new Error("Only -p flag is supported for now ");
+  }
+  if (process.argv.length < 5) {
+    throw new Error("Command option missing (-p)");
+  }
+  const hash = process.argv[4];
+  const zipData = await fs.readFile(
+    path(__dirname, ".git", "objects", hash.slice(0, 2), hash.slice(2))
+  );
+  const extractedData = zlib.inflateSync(zipData);
+  const [, content] = extractedData.toString().split("\x00");
+  process.stdout.write(content);
 }
