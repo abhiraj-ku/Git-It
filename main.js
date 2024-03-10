2edeba941003e79f8067d1002cfbb99d4d1d5dc8
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-03-10 18:28:09 +0530

    [feature 3] Add support for creating hash of the object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
-
+const crypto = require("crypto");
 const command = process.argv[2];
 
 switch (command) {
@@ -10,6 +10,9 @@ switch (command) {
     break;
   case "cat-file":
     catFile();
+    break;
+  case "hash-object":
+    hashObject();
     break;
   default:
     throw new Error(`Unknown command: ${command}`);
@@ -41,4 +44,18 @@ async function catFile() {
   const extractedData = zlib.inflateSync(zipData);
   const [, content] = extractedData.toString().split("\x00");
   process.stdout.write(content);
+}
+
+function hashObject() {
+  const command = process.argv[3];
+  if (command !== "-w") return;
+  const file = process.argv[4];
+  const content = fs.readFileSync(file);
+  const header = `blob ${content.length}\x00`;
+  const data = header + content;
+  const hash = crypto.createHash("sha1").update(data).digest("hex");
+  const hashDirPath = path.join(objectsDirPath, hash.slice(0, 2));
+  const filePath = path.join(hashDirPath, hash.slice(2));
+  fs.mkdirSync(hashDirPath, { recursive: true });
+  process.stdout.write(hash);
 }
